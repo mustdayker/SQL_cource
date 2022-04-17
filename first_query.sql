45733c75decbf84dfe7647f25796a3281667e6f5
Dmitry Kosarev <mustdayker@gmail.com>
2022-04-17 15:52:30 +0300

    Update first_query.sql

--- first_query.sql
+++ first_query.sql
@@ -1,1 +1,20 @@
-SELECT DISTINCT job_id, department_id FROM job_history;
+SELECT DISTINCT 
+job_id, department_id 
+FROM job_history;
+
+SELECT salary*2+employee_id FROM employees;
+
+SELECT first_name, salary*1.5, salary FROM employees;
+
+SELECT start_date, end_date, end_date-start_date+1 FROM job_history;
+
+SELECT start_date, start_date+7 FROM job_history;
+
+SELECT * FROM employees;
+
+SELECT first_name||' '||last_name FROM employees;
+
+SELECT first_name||' '||last_name FL_Name FROM employees;
+
+SELECT first_name nnn FROM employees;
+
